df8e51770def1edcf3e8dc575e463824f7523038
Ben Davidson <ben@bendavidson.me>
2017-04-03 15:21:10 -0400

    Updated "General" modules

--- field-groups/modules/20-general/210-columned-content.php
+++ field-groups/modules/20-general/210-columned-content.php
@@ -3,7 +3,46 @@
 acf_add_local_field_group(array (
   'key' => 'group_58d98e060fcd1',
   'title' => '210 - Columned Content',
-  'fields' => false,
+  'fields' => array (
+    array (
+      'key' => 'field_58dab3b4f5c8e',
+      'label' => 'Headline',
+      'name' => 'columned-content_headline',
+      'type' => 'text',
+      'instructions' => '',
+      'required' => 0,
+      'conditional_logic' => 0,
+      'wrapper' => array (
+        'width' => '',
+        'class' => 'columned-content columned-content__headline',
+        'id' => '',
+      ),
+      'default_value' => '',
+      'placeholder' => '',
+      'prepend' => '',
+      'append' => '',
+      'maxlength' => '',
+    ),
+    array (
+      'key' => 'field_58dab3f8f5c8f',
+      'label' => 'Content',
+      'name' => 'columned-content_body',
+      'type' => 'wysiwyg',
+      'instructions' => '',
+      'required' => 0,
+      'conditional_logic' => 0,
+      'wrapper' => array (
+        'width' => '',
+        'class' => 'columned-content columned-content__body',
+        'id' => '',
+      ),
+      'default_value' => '',
+      'tabs' => 'all',
+      'toolbar' => 'full',
+      'media_upload' => 0,
+      'delay' => 0,
+    ),
+  ),
   'location' => array (
     array (
       array (
--- field-groups/modules/20-general/220-linked-photo-grid.php
+++ field-groups/modules/20-general/220-linked-photo-grid.php
@@ -3,7 +3,69 @@
 acf_add_local_field_group(array (
   'key' => 'group_58d98e5159b2c',
   'title' => '220 - Linked Photo Grid',
-  'fields' => false,
+  'fields' => array (
+    array (
+      'key' => 'field_58dfb7806fa01',
+      'label' => 'Linked Photos',
+      'name' => 'linked_photos',
+      'type' => 'repeater',
+      'instructions' => '',
+      'required' => 0,
+      'conditional_logic' => 0,
+      'wrapper' => array (
+        'width' => '',
+        'class' => 'linked-photos',
+        'id' => '',
+      ),
+      'collapsed' => '',
+      'min' => 0,
+      'max' => 0,
+      'layout' => 'block',
+      'button_label' => 'Add Item',
+      'sub_fields' => array (
+        array (
+          'key' => 'field_58dfb7b26fa02',
+          'label' => 'Image',
+          'name' => 'linked-photos_image',
+          'type' => 'image',
+          'instructions' => '',
+          'required' => 0,
+          'conditional_logic' => 0,
+          'wrapper' => array (
+            'width' => '30',
+            'class' => 'linked-photos linked-photos__image',
+            'id' => '',
+          ),
+          'return_format' => 'url',
+          'preview_size' => 'thumbnail',
+          'library' => 'all',
+          'min_width' => '',
+          'min_height' => '',
+          'min_size' => '',
+          'max_width' => '',
+          'max_height' => '',
+          'max_size' => '',
+          'mime_types' => '',
+        ),
+        array (
+          'key' => 'field_58dfb8016fa03',
+          'label' => 'URL',
+          'name' => 'linked-photos_url',
+          'type' => 'url',
+          'instructions' => '',
+          'required' => 0,
+          'conditional_logic' => 0,
+          'wrapper' => array (
+            'width' => '70',
+            'class' => 'linked-photos linked-photos__url',
+            'id' => '',
+          ),
+          'default_value' => '',
+          'placeholder' => '',
+        ),
+      ),
+    ),
+  ),
   'location' => array (
     array (
       array (
@@ -19,7 +81,7 @@ acf_add_local_field_group(array (
   'label_placement' => 'top',
   'instruction_placement' => 'label',
   'hide_on_screen' => '',
-  'active' => 0,
+  'active' => 1,
   'description' => '',
 ));
 

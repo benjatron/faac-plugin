c0cf257dfc967e3fbc7ae8a88f1ad986063167fe
Ben Davidson <ben@bendavidson.me>
2017-04-03 12:56:31 -0400

    Updated Division Category page template

--- page-templates/divisions-category.php
+++ page-templates/divisions-category.php
@@ -1,3 +1,92 @@
 <?php
 
+acf_add_local_field_group(array (
+  'key' => 'group_58e27dd7a7e8b',
+  'title' => 'Division Page, Category (D5)',
+  'fields' => array (
+    array (
+      'key' => 'field_58e27dd7abae5',
+      'label' => 'Basic Slider',
+      'name' => 'division-category_slider',
+      'type' => 'clone',
+      'instructions' => '',
+      'required' => 0,
+      'conditional_logic' => 0,
+      'wrapper' => array (
+        'width' => '',
+        'class' => '',
+        'id' => '',
+      ),
+      'clone' => array (
+        0 => 'group_58d31322a60eb',
+      ),
+      'display' => 'group',
+      'layout' => 'block',
+      'prefix_label' => 0,
+      'prefix_name' => 1,
+    ),
+    array (
+      'key' => 'field_58e27dd7abb20',
+      'label' => 'Intro Content',
+      'name' => 'division-category_intro',
+      'type' => 'clone',
+      'instructions' => '',
+      'required' => 0,
+      'conditional_logic' => 0,
+      'wrapper' => array (
+        'width' => '',
+        'class' => '',
+        'id' => '',
+      ),
+      'clone' => array (
+        0 => 'group_58d32e8d672b3',
+      ),
+      'display' => 'group',
+      'layout' => 'block',
+      'prefix_label' => 0,
+      'prefix_name' => 1,
+    ),
+    array (
+      'key' => 'field_58e27dd7abb57',
+      'label' => 'Secondary Content',
+      'name' => 'division-category_secondary-content',
+      'type' => 'clone',
+      'instructions' => '',
+      'required' => 0,
+      'conditional_logic' => 0,
+      'wrapper' => array (
+        'width' => '',
+        'class' => '',
+        'id' => '',
+      ),
+      'clone' => array (
+        0 => 'group_58d98e5e60691',
+      ),
+      'display' => 'group',
+      'layout' => 'block',
+      'prefix_label' => 0,
+      'prefix_name' => 1,
+    ),
+  ),
+  'location' => array (
+    array (
+      array (
+        'param' => 'page_template',
+        'operator' => '==',
+        'value' => 'template-divisions-category.php',
+      ),
+    ),
+  ),
+  'menu_order' => 0,
+  'position' => 'normal',
+  'style' => 'default',
+  'label_placement' => 'top',
+  'instruction_placement' => 'label',
+  'hide_on_screen' => array (
+    0 => 'the_content',
+  ),
+  'active' => 1,
+  'description' => '',
+));
+
 ?>

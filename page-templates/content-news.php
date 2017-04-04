6faff335076d6bc652794600cbb6fdffa937aee2
Ben Davidson <ben@bendavidson.me>
2017-04-03 22:26:08 -0400

    Updated page template layouts

--- page-templates/content-news.php
+++ page-templates/content-news.php
@@ -1,3 +1,136 @@
 <?php
 
+acf_add_local_field_group(array (
+  'key' => 'group_58e300d2064cb',
+  'title' => 'Content Page, News (6)',
+  'fields' => array (
+    array (
+      'key' => 'field_58e300d20a4da',
+      'label' => 'Slider',
+      'name' => 'content-general_slider',
+      'type' => 'clone',
+      'instructions' => '',
+      'required' => 0,
+      'conditional_logic' => 0,
+      'wrapper' => array (
+        'width' => '',
+        'class' => '',
+        'id' => '',
+      ),
+      'clone' => array (
+        0 => 'group_58d31322a60eb',
+      ),
+      'display' => 'group',
+      'layout' => 'block',
+      'prefix_label' => 0,
+      'prefix_name' => 1,
+    ),
+    array (
+      'key' => 'field_58e300d20a5a1',
+      'label' => 'Linked Photos',
+      'name' => 'content-general_linked-photos',
+      'type' => 'clone',
+      'instructions' => '',
+      'required' => 0,
+      'conditional_logic' => 0,
+      'wrapper' => array (
+        'width' => '',
+        'class' => '',
+        'id' => '',
+      ),
+      'clone' => array (
+        0 => 'group_58da7eaa88e52',
+      ),
+      'display' => 'group',
+      'layout' => 'block',
+      'prefix_label' => 0,
+      'prefix_name' => 1,
+    ),
+    array (
+      'key' => 'field_58e300d20a5e5',
+      'label' => 'Categories',
+      'name' => 'content-general_categories',
+      'type' => 'clone',
+      'instructions' => '',
+      'required' => 0,
+      'conditional_logic' => 0,
+      'wrapper' => array (
+        'width' => '',
+        'class' => '',
+        'id' => '',
+      ),
+      'clone' => array (
+        0 => 'group_58da7eb43a340',
+      ),
+      'display' => 'group',
+      'layout' => 'block',
+      'prefix_label' => 0,
+      'prefix_name' => 1,
+    ),
+    array (
+      'key' => 'field_58e300d20a620',
+      'label' => 'Call-to-Action',
+      'name' => 'content-general_cta',
+      'type' => 'clone',
+      'instructions' => '',
+      'required' => 0,
+      'conditional_logic' => 0,
+      'wrapper' => array (
+        'width' => '',
+        'class' => '',
+        'id' => '',
+      ),
+      'clone' => array (
+        0 => 'group_58da813eaaaad',
+      ),
+      'display' => 'group',
+      'layout' => 'block',
+      'prefix_label' => 0,
+      'prefix_name' => 1,
+    ),
+    array (
+      'key' => 'field_58e300d20a64e',
+      'label' => 'Footer',
+      'name' => 'solutions-child_footer',
+      'type' => 'clone',
+      'instructions' => '',
+      'required' => 0,
+      'conditional_logic' => 0,
+      'wrapper' => array (
+        'width' => '',
+        'class' => '',
+        'id' => '',
+      ),
+      'clone' => array (
+        0 => 'group_58da816050824',
+        1 => 'group_58da81605c4c2',
+        2 => 'group_58da8160620c4',
+      ),
+      'display' => 'group',
+      'layout' => 'block',
+      'prefix_label' => 0,
+      'prefix_name' => 1,
+    ),
+  ),
+  'location' => array (
+    array (
+      array (
+        'param' => 'page_template',
+        'operator' => '==',
+        'value' => 'template-solutions-child.php',
+      ),
+    ),
+  ),
+  'menu_order' => 0,
+  'position' => 'normal',
+  'style' => 'default',
+  'label_placement' => 'top',
+  'instruction_placement' => 'label',
+  'hide_on_screen' => array (
+    0 => 'the_content',
+  ),
+  'active' => 1,
+  'description' => '',
+));
+
 ?>

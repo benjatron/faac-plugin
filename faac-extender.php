a0346bbc5f314838e416ec3323efbff509a51b1e
Ben Davidson <ben@bendavidson.me>
2017-04-10 12:36:35 -0400

    Removed problematic action

--- faac-extender.php
+++ faac-extender.php
@@ -266,34 +266,32 @@ class newsletter_widget extends WP_Widget {
   function newsletter_widget() {
   parent::WP_Widget(false, $name = __('Newsletter Signup', 'faac_widget_newsletterSignup') );
 }
-add_action('widgets_init', create_function('', 'return register_widget("newsletter_widget");'));
 
 class masthead_widget extends WP_Widget {
   function masthead_widget() {
   parent::WP_Widget(false, $name = __('Brand Masthead', 'faac_widget_masthead') );
 }
-add_action('widgets_init', create_function('', 'return register_widget("masthead_widget");'));
+
 
 class linkButtons_widget extends WP_Widget {
   function linkButtons_widget() {
   parent::WP_Widget(false, $name = __('Link Buttons', 'faac_widget_linkButtons') );
 }
-add_action('widgets_init', create_function('', 'return register_widget("linkButtons_widget");'));
+
 
 class divisionLogos_widget extends WP_Widget {
   function divisionLogos_widget() {
   parent::WP_Widget(false, $name = __('Linked Division Logos', 'faac_widget_divisionLogos') );
 }
-add_action('widgets_init', create_function('', 'return register_widget("divisionLogos_widget");'));
+
 
 class contactInfo_widget extends WP_Widget {
   function contactInfo_widget() {
   parent::WP_Widget(false, $name = __('Contact Information Blocks', 'faac_widget_contactInfo') );
 }
-add_action('widgets_init', create_function('', 'return register_widget("contactInfo_widget");'));
+
 
 class copyright_widget extends WP_Widget {
   function copyright_widget() {
   parent::WP_Widget(false, $name = __('CopyrightInfo', 'faac_widget_copyright') );
 }
-add_action('widgets_init', create_function('', 'return register_widget("copyright_widget");'));

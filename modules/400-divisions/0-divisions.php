c50d7f12e963f19a875eee54704825afc098bd83
Ben Davidson <ben@bendavidson.me>
2017-05-03 14:27:56 -0400

    Added division feature row

--- modules/400-divisions/0-divisions.php
+++ modules/400-divisions/0-divisions.php
@@ -3,5 +3,6 @@
 include_once('400-division-summaries.php');
 include_once('410-division-feature-block.php');
 include_once('420-division-support-block.php');
+include_once('430-division-feature-row.php');
 
 ?>

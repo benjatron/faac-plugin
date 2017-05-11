aee583356581197a1c20d5668252d71b232bf91b
Ben Davidson <ben@bendavidson.me>
2017-05-11 13:14:09 -0400

    Added taxonomies to attachments

--- taxonomies/categories.php
+++ taxonomies/categories.php
@@ -33,7 +33,7 @@ function page_category() {
         'show_in_nav_menus'          => true,
         'show_tagcloud'              => true,
     );
-    register_taxonomy( 'category', array( 'post', 'page' ), $args );
+    register_taxonomy( 'category', array( 'post', 'page', 'attachment' ), $args );
 
 }
 add_action( 'init', 'page_category', 0 );
